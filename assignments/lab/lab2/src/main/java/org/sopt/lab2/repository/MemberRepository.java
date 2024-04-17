916768095c0dfe145c2a733377e3ccaf1dafbb9b
bbabbi <mooncheayoung@gmail.com>
2024-04-17 20:28:06 +0900

    Revert "#4 feat : 2주차 세미나 진행 및 과제 진행(Member 생성 조회 삭제에 대한 API 구현)"
    This reverts commit 8308383fc629d6cf7c18bc79bd4bceeb419a545b.

--- assignments/lab/lab2/src/main/java/org/sopt/lab2/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.lab2.repository;
-
-import org.sopt.lab2.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
